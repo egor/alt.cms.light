52581d06cd822de3593145055a1a5087257f593d
Egor Rihnov <egor.developer@gmail.com>
2013-02-22 15:39:22 +0200

    Админка. Управление пользователями

---
+++ www/protected/modules/altadmin/views/default/login.php
@@ -0,0 +1,27 @@
+<?php
+/* @var $this DefaultController */
+
+$this->breadcrumbs=array(
+	$this->module->id,
+);
+?>
+<h1><?php echo $this->uniqueId . '/' . $this->action->id; ?></h1>
+<?php /** @var BootActiveForm $form */
+$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+    'id'=>'verticalForm',    
+    'htmlOptions'=>array('class'=>'well span3'),
+)); ?>
+ 
+<?php echo $form->textFieldRow($model, 'username', array('class'=>'span3')); ?>
+<?php echo $form->passwordFieldRow($model, 'password', array('class'=>'span3')); ?>
+<?php echo $form->checkboxRow($model, 'rememberMe'); ?>
+<?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'type'=>'primary', 'label'=>'Вход')); ?>
+<?php $this->endWidget(); ?>
+<p>
+This is the view content for action "<?php echo $this->action->id; ?>".
+The action belongs to the controller "<?php echo get_class($this); ?>"
+in the "<?php echo $this->module->id; ?>" module.
+</p>
+<p>
+You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
+</p>
